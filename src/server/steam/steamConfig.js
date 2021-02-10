53202be58ef65a2cbc2387f2bbd30087ccacdfc2
Roozbeh Moradi <roozbehmoradi@Roozbehs-MBP.home>
2021-02-10 11:05:45 +0100

    ..

--- src/server/steam/steamConfig.js
+++ src/server/steam/steamConfig.js
@@ -12,7 +12,7 @@ module.exports = {
 	/**This is a fake api key for illustration only so you have to replace with your own. 
 	 * Warning: Never share your api key publically!
 	 */
-	apiKey: '4228E6E6084EFF47FADFASDSAF32750D'  
+	apiKey: '4228E6E6084EFF471B3AE36095sffssdfsd'  
 
 	
 }   
